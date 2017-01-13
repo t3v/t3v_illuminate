02aec68da9fef01b4a1079fac1d2cf20100b329d
Maik Kempe <mkempe@bitaculous.com>
2017-01-13 18:03:52 +0100

    [➠] Updated `ext_emconf.php`. [➠] Updated Composer configuration. [➠] Updated RELEASE.

--- ext_emconf.php
+++ ext_emconf.php
@@ -8,11 +8,9 @@ $EM_CONF[$_EXTKEY] = [
   'category' => 'be',
   'state' => 'stable',
   'version' => '0.0.1',
-  'shy' => false,
   'createDirs' => '',
   'uploadfolder' => false,
-  'modify_tables' => '',
-  'clearCacheOnLoad' => true,
+  'clearCacheOnLoad' => false,
   'constraints' => [
     'depends' => [
       'typo3' => '7.6.0-8.1.99',
@@ -21,5 +19,15 @@ $EM_CONF[$_EXTKEY] = [
     'conflicts' => [
     ],
     'suggests' => []
+  ],
+  'autoload' => [
+    'psr-4' => [
+      'T3v\\T3vIlluminate\\' => 'Classes'
+    ]
+  ],
+  'autoload-dev' => [
+    'psr-4' => [
+      'T3v\\T3vIlluminate\\Tests\\' => 'Tests'
+    ]
   ]
 ];
