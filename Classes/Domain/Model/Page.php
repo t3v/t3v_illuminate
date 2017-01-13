0be8606a07aba82b2f7f8b4a8d97805435b43216
Maik Kempe <mkempe@bitaculous.com>
2017-01-13 21:31:31 +0100

    [➠] Added `Page` and `PageLanguageOverlay` model.

---
+++ Classes/Domain/Model/Page.php
@@ -0,0 +1,18 @@
+<?php
+namespace T3v\T3vIlluminate\Domain\Model;
+
+use \T3v\T3vIlluminate\Domain\Model\AbstractModel;
+
+/**
+ * Page Class
+ *
+ * @package T3v\T3vIlluminate\Domain\Model
+ */
+class Page extends AbstractModel {
+  /**
+   * The table associated with the model.
+   *
+   * @var string
+   */
+  protected $table = 'pages';
+}
---
+++ Classes/Domain/Model/PageLanguageOverlay.php
@@ -0,0 +1,18 @@
+<?php
+namespace T3v\T3vIlluminate\Domain\Model;
+
+use \T3v\T3vIlluminate\Domain\Model\AbstractModel;
+
+/**
+ * Page Language Overlay Class
+ *
+ * @package T3v\T3vIlluminate\Domain\Model
+ */
+class PageLanguageOverlay extends AbstractModel {
+  /**
+   * The table associated with the model.
+   *
+   * @var string
+   */
+  protected $table = 'pages_language_overlay';
+}
