5ade3221ca8ddac23036494baa8c66fa6054da83
Maik Kempe <mkempe@bitaculous.com>
2017-01-13 21:31:14 +0100

    [✓] Fixed `AbstractModel`.

--- Classes/Domain/Model/AbstractModel.php
+++ Classes/Domain/Model/AbstractModel.php
@@ -4,11 +4,11 @@ namespace T3v\T3vIlluminate\Domain\Model;
 use \Illuminate\Database\Eloquent\Model;
 
 /**
- * Abstract Entity Class
+ * Abstract Model Class
  *
  * @package T3v\T3vIlluminate\Domain\Model
  */
-abstract class AbstractEntity extends Model {
+abstract class AbstractModel extends Model {
   const CREATED_AT = 'crdate';
   const UPDATED_AT = 'tstamp';
 
