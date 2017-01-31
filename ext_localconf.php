053fc9351a449e9c7fe330c411ce3a4b52d6e5d0
Maik Kempe <mkempe@bitaculous.com>
2017-01-31 14:18:53 +0100

    [➠] Use `call_user_func` in `ext_localconf.php` and `ext_tables.php`.

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,8 +1,10 @@
 <?php
 defined('TYPO3_MODE') or die('Access denied.');
 
-// === Commands ===
+call_user_func(function ($extkey) {
+  // === Commands ===
 
-if (TYPO3_MODE === 'BE') {
-  $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['extbase']['commandControllers'][$_EXTKEY] = \T3v\T3vIlluminate\Command\Page\LanguageOverlayCommandController::class;
-}
+  if (TYPO3_MODE === 'BE') {
+    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['extbase']['commandControllers'][$extkey] = \T3v\T3vIlluminate\Command\Page\LanguageOverlayCommandController::class;
+  }
+}, $_EXTKEY);
